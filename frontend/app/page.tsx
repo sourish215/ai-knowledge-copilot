27f9c351c0d18fac9fa69eb4481c8cfed3755818
sourish215 <sourish215@gmail.com>
2026-03-05 15:53:24 +0530

    fix: chat container scroll

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -175,7 +175,7 @@ export default function Chat() {
 
 
         {/* CHAT AREA */}
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col min-h-0">
 
           {/* Messages */}
           <div className="flex-1 overflow-y-auto p-6 space-y-4">
